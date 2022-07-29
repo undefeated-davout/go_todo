ed862013e585276cf283274c290346ec6e3bed46
undefeated-davout <34504518+undefeated-davout@users.noreply.github.com>
2022-07-29 10:56:37 +0900

    update

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/undefeated-davout/go_todo/config"
 	"golang.org/x/sync/errgroup"
@@ -20,6 +23,8 @@ func main() {
 }
 
 func run(ctx context.Context) error {
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	defer stop()
 	cfg, err := config.New()
 	if err != nil {
 		return err
@@ -34,6 +39,8 @@ func run(ctx context.Context) error {
 		// 引数で受け取ったnet.Listenerを利用するので、
 		// Addrフィールドは指定しない
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// コマンドラインで実験するため
+			time.Sleep(5 * time.Second)
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
 	}
