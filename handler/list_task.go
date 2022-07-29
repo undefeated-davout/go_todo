623fa26c04069657858b19d18c5c59cf983f7a44
undefeated-davout <34504518+undefeated-davout@users.noreply.github.com>
2022-07-29 14:38:06 +0900

    update

---
+++ handler/list_task.go
@@ -0,0 +1,32 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/undefeated-davout/go_todo/entity"
+	"github.com/undefeated-davout/go_todo/store"
+)
+
+type ListTask struct {
+	Store *store.TaskStore
+}
+
+type task struct {
+	ID     entity.TaskID `json:"id"`
+	Title  string        `json:"title"`
+	Status string        `json:"status"`
+}
+
+func (lt *ListTask) ServerHTTP(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	tasks := lt.Store.All()
+	rsp := []task{}
+	for _, t := range tasks {
+		rsp = append(rsp, task{
+			ID:     t.ID,
+			Title:  t.Title,
+			Status: t.Status,
+		})
+	}
+	RespondJSON(ctx, w, rsp, http.StatusOK)
+}
