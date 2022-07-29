bf2c40c7de3dd4bb58c3a8291ee210da293c830f
undefeated-davout <34504518+undefeated-davout@users.noreply.github.com>
2022-07-29 19:23:33 +0900

    update

---
+++ clock/clock.go
@@ -0,0 +1,21 @@
+package clock
+
+import (
+	"time"
+)
+
+type Clocker interface {
+	Now() time.Time
+}
+
+type RealClocker struct{}
+
+func (r RealClocker) Now() time.Time {
+	return time.Now()
+}
+
+type FixedClocker struct{}
+
+func (fc FixedClocker) Now() time.Time {
+	return time.Date(2000, 1, 2, 10, 20, 30, 0, time.UTC)
+}
--- go.mod
+++ go.mod
@@ -8,7 +8,9 @@ require (
 	github.com/caarlos0/env/v6 v6.9.3
 	github.com/go-chi/chi/v5 v5.0.7
 	github.com/go-playground/validator/v10 v10.11.0
+	github.com/go-sql-driver/mysql v1.6.0
 	github.com/google/go-cmp v0.5.8
+	github.com/jmoiron/sqlx v1.3.5
 )
 
 require (
